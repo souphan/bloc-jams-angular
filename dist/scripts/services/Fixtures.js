97387eccb9ab75d7b7244cc8a2b3bff80e25c3c3
System Administrator <root@souphans-MacBook.local>
2016-01-17 08:51:39 -0600

    added play and pause buttons on album

--- dist/scripts/services/Fixtures.js
+++ dist/scripts/services/Fixtures.js
@@ -44,9 +44,13 @@
         };
          
             return Fixtures;
-        }
+        
  
-     angular
+     
+  angular
          .module('blocJams')
          .factory('Fixtures', Fixtures);
  })();
+
+
+
