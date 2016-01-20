ec125ff80cdde6fb1da8c59b7acad3b5ab943dc1
Souphan <souphan.ctr@gmail.com>
2016-01-19 18:19:51 -0600

    updated assets and albumctrl.js

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -8,27 +8,3 @@
          .module('blocJams')
          .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
  })();
-
-/*
-(function() {
-     function AlbumCtrl(Fixtures) {
-         this.albumData = angular.copy(albumPicasso)
-         }
-(function() {
-     function AlbumCtrl() {
-         this.albumData = albumPicasso;
-         
-     }
- 
-     angular
-         .module('blocJams')
-         .controller('AlbumCtrl', AlbumCtrl);
- })();
-
-
-         function CollectionCtrl() {
-     this.albums = [];
-     for (var i=0; i < 12; i++) {
-         this.albums.push(angular.copy(albumPicasso));
-     }
- }*/
